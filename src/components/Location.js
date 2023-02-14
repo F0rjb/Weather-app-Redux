e8dd95934fa01eb1844cb3a7c867077688229ece
Ahmed Nasri <f0rjb>
2023-02-14 18:51:25 +0100

    this is nearly done 2

--- src/components/Location.js
+++ src/components/Location.js
@@ -17,47 +17,14 @@ import { useParams } from 'react-router-dom';
 import { getCurrentData } from './JS/actions/actions';
 export const Locations = () => {
   const { name } = useParams();
-  console.log(name);
-  const HandleClick = () => {
-    // dispatch(select_location(Location));
-  };
+
   console.log(name);
 
-  // const URL = `http://api.weatherapi.com/v1/forecast.json?key=632ce770018b45c0a9e180343230302&&q=${name}&days=4&aqi=no&alerts=no
-  // `;
   const data = useSelector(state => state.location.data);
   const onLoading = useSelector(state => state.location.onLoading);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getCurrentData(name));
-
-    // const fetchData = async () => {
-    //   try {
-    //     const result = await axios(URL);
-    //     setAPIDATA(result.data.location);
-    //     setdata.current(result.data.current);
-    //     // setdata.forecast(result.data.forecast.forecastday);
-    //     setToggle(false);
-    //     setdata.forecast(result.data.forecast);
-    //     console.log(data.forecast.forecastday);
-    //   } catch (error) {
-    //     setError(error);
-    //   }
-    // };
-    // console.log(data.forecast);
-
-    // var requestOptions = {
-    //   method: 'GET',
-    // };
-
-    // fetch(
-    //   'https://api.geoapify.com/v2/places?categories=commercial.supermarket&filter=rect%3A10.716463143326969%2C48.755151258420966%2C10.835314015356737%2C48.680903341613316&limit=20&apiKey=967386f36631440ea67e641eccb96b65',
-    //   requestOptions
-    // )
-    //   .then(response => response.json())
-    //   .then(result => console.log(result))
-    //   .catch(error => console.log('error', error));
-    // fetchData();
   }, [name]);
 
   return (
@@ -103,15 +70,11 @@ export const Locations = () => {
                 },
               }}
             >
-              {/* <h1>Location: {APIDATA.name}</h1>
-            <h1>Region: {APIDATA.region ? APIDATA.region : APIDATA.name}</h1>
-            <h1>Temprature: {data.current.temp_c} °C</h1>
-            <h1>
-              {data.current.condition.text}: <img src={data.current.condition.icon} alt="" />
-            </h1> */}
               {data.forecast.forecastday.map((el, key) => (
                 <div>
+                  {console.log(el)}
                   <p>{el.date}</p>
+                  <p>{el.day.avgtemp_c} Degree Celcius</p>
                 </div>
               ))}
             </CardFooter>
